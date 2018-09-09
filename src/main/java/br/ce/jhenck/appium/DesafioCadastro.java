257fb657e17e0afeee23f93d73a3b575fa88e1c1
Unknown <jcarboni2@gmail.com>
2018-09-09 17:30:00 -0300

    Ajustar classe

--- src/main/java/br/ce/jhenck/appium/DesafioCadastro.java
+++ src/main/java/br/ce/jhenck/appium/DesafioCadastro.java
@@ -7,32 +7,32 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;;
 
 public class DesafioCadastro {
 
 	@Test
 	public void deveCadastrar() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("udid", "192.168.11.101:5555");
+		desiredCapabilities.setCapability("udid", "emulator-5554");
 		desiredCapabilities.setCapability("platformName", "Android");
 		desiredCapabilities.setCapability("deviceName", "Qualquer");
 		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability("MobileCapabilityType.APP",
-				"/Users/Johnny/eclipse-workspace/CursoAppium/src/main/resources/CTAppium-1-1.apk");
+		desiredCapabilities.setCapability(MobileCapabilityType.APP,
+				"D:/Cursos e Treinamentos/Curso Android com Appium/CursoAppium/src/main/resources/CTAppium-1-1.apk");
 
 		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"),
 				desiredCapabilities);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+		
 		// Selecionar formulário
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']")).click();
-
+		
 		// Preencher campo
 		driver.findElement(By.xpath("//android.widget.EditText[@text='Nome']")).sendKeys("Manoel dos Santos");
 
@@ -41,13 +41,6 @@ public class DesafioCadastro {
 
 		// Select opção
 		driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='PS4']")).click();
-
-		// Deslizar barra - PENDENTE!!!
-//		MobileElement seekBar = driver.findElement(MobileBy.AccessibilityId("slid"));
-//		seekBar.sendKeys("8");
-//		Action dragDrop = TouchAction();
-//		dragDrop.DragAndDrop(seekBar.Location, new Location(){seekBar.Location.X + (seekBar.Size.Width / 2), seekBar.Location.Y + (seekBar.Size.Height / 2)});
-//		dragDrop.Perform();
 
 		// Verificar status, clicar e validar status alterado
 		MobileElement check = driver.findElement(By.className("android.widget.CheckBox"));
@@ -59,15 +52,10 @@ public class DesafioCadastro {
 		Assert.assertFalse(check.getAttribute("checked").equals("false"));
 		Assert.assertFalse(switc.getAttribute("checked").equals("true"));
 
-		// Informar a data
-		driver.findElement(By.className("android.view.ViewGroup")).sendKeys("01/08/2018");
-
-		// Informar a hora
-		driver.findElement(By.className("android.view.ViewGroup")).sendKeys("15:00");
-
 		// Salvar cadastro
-		driver.findElement(By.className("android.widget.TextView")).click();
-
+		driver.findElement(By.xpath("//*[@text='SALVAR']")).click();
+		
+		// Close all browsers
 		driver.quit();
 
 	}
